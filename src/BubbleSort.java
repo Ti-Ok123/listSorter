6fbcafa849535b5b75008a9c0a63487756829f68
koyuncay1 <ayse.koyuncu@frg.wwschool.de>
2025-01-31 08:33:03 +0100

    bubble sort

--- src/BubbleSort.java
+++ src/BubbleSort.java
@@ -12,11 +12,14 @@ public class BubbleSort<ContentType extends ComparableContent<ContentType>> {
     public void bubbleSort() {
         list.toFirst();
         while (list.hasAccess()){
-
+            Co
+            if(list.getContent().isEqual()){}
         }
     }
 //es muss noch tauschen und irgendwie die stellen in der liste finden
     public static <ContentType> void swap(List<ContentType> list, ContentType item1, ContentType item2) {
         ContentType ersatz = item1;
+        item1 = item2;
+        item2 = ersatz;
     }
 }
