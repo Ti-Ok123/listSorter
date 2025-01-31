db9815e3dc961a0bd85578b14ce90908d6026b00
pinkni <niklas.pink@frg.wwschool.de>
2025-01-31 09:32:02 +0100

    ok

--- src/BubbleSort.java
+++ src/BubbleSort.java
@@ -1,19 +1,58 @@
 import utils.ComparableContent;
 import utils.List;
 
+import java.security.cert.CertStoreException;
+
 public class BubbleSort<ContentType extends ComparableContent<ContentType>> {
     private List<ContentType> list;
 
     public BubbleSort(List<ContentType> pList) {
+
         this.list = pList;
     }
 
 
-    public void bubbleSort() {
-
+    public boolean bubbleSort() {
+        ContentType Erstes;
+        ContentType Zweites;
+        int a = 0;
+        list.toFirst();
+        while (list.hasAccess()) {
+            if (list.getContent() == null) {
+                return false;
+            } else if (list.getContent() != null) {
+                list.toFirst();
+                Erstes = list.getContent();
+                list.next();
+                a++;
+                Zweites = list.getContent();
+                if (Zweites > Erstes) {
+                    swap(list, Zweites, Erstes);
+                }
+            }
+        }
+        return false;
     }
 
-    public static <ContentType> void swap(List<ContentType> list, ContentType item1, ContentType item2) {
 
+    public static <ContentType> void swap(List<ContentType> list, ContentType item1, ContentType item2) {
+        ContentType pos1;
+        ContentType pos2;
+        pos1 = item1;
+        pos2 = item2;
+        list.toFirst();
+        while (list.hasAccess()) {
+            if (list.getContent() == item2) {
+                list.setContent(pos1);
+            }
+            list.next();
+        }
+        list.toFirst();
+        while (list.hasAccess()) {
+            if (list.getContent() == item1) {
+                list.setContent(pos2);
+            }
+            list.next();
+        }
     }
 }
