93af574cdd3dadb679f278501a91f6e010e6d9c8
hannah.blech <hannah.blech@web.de>
2025-02-03 08:43:39 +0100

    llk>

--- src/BubbleSort.java
+++ src/BubbleSort.java
@@ -11,25 +11,32 @@ public class BubbleSort<ContentType extends ComparableContent<ContentType>> {
 
     public void bubbleSort() {
         int lenge = lengeListe();
-        ContentType a;
-        ContentType b;
+        System.out.println(lenge);
+        ContentType a = null;
+        ContentType b = null;
 
         for(int i = 0; i < lenge; i++){
             list.toFirst();
-            a = list.getContent();
+
             //System.out.println("a ist"+ a);
-            list.next();
-            b = list.getContent();
+
             //System.out.println("b ist"+ b);
-            for(int j = 0; j < lenge ; j++){
-                System.out.println("a ist"+ a+ " und i ist"+j);
-                System.out.println("b ist"+ b+" und j ist "+ (j+1));
-                if(a.isGreater(b)){
-                    swap(list, a,j,b, (j+1));
-                }
+            for(int j = 0; j < lenge -i ; j++){
                 a = list.getContent();
                 list.next();
                 b = list.getContent();
+                System.out.println("a ist "+ a+ " und i ist"+j);
+                System.out.println("b ist "+ b+" und j ist "+ (j+1));
+                if(a.isGreater(b)){
+                    swap(list, a,b);
+                    list.toFirst();
+                    for(int k = 0; k< j; k++){
+                        list.next();
+                    }
+                }
+                //a = list.getContent();
+                //list.next();
+                //b = list.getContent();
             }
 
         }
@@ -42,20 +49,42 @@ public class BubbleSort<ContentType extends ComparableContent<ContentType>> {
             lenge++;
             list.next();
         }
+        list.toFirst();
         return lenge;
+
     }
 
-    public <ContentType> void swap(List<ContentType> list, ContentType item1,int indexa, ContentType item2, int indexb) {
+    public <ContentType> void swap(List<ContentType> list, ContentType item1, ContentType item2) {
         list.toFirst();
-        for(int i = 0; i < indexa; i++){
+         if (list.isEmpty() || item1.equals(item2)) {
+            return; // No need to swap if the list is empty or both items are the same
+        }
+
+        list.toFirst();
+        boolean foundItem1 = false, foundItem2 = false;
+
+        // Find both elements
+        while (list.hasAccess()) {
+            if (list.getContent().equals(item1)) {
+                foundItem1 = true;
+            } else if (list.getContent().equals(item2)) {
+                foundItem2 = true;
+            }
             list.next();
         }
-        list.setContent(item2);
-        list.toFirst();
-        for(int i = 0; i < indexb; i++){
-            list.next();
+
+        // If both elements are found, swap them
+        if (foundItem1 && foundItem2) {
+            list.toFirst();
+            while (list.hasAccess()) {
+                if (list.getContent().equals(item1)) {
+                    list.setContent(item2);
+                } else if (list.getContent().equals(item2)) {
+                    list.setContent(item1);
+                }
+                list.next();
+            }
         }
-        list.setContent(item1);
         list.toFirst();
-        for(int i = 0; i< indexa; i++){list.next();}
+
 }}
