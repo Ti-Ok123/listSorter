b91b53d23400e2571d01091dd4b73bd03289ebd6
aysek <aysea.koyuncu1705@gmail.com>
2025-02-03 22:20:41 +0100

    selection s funktioniert, wieso auch ímmer

--- src/SelectionSort.java
+++ src/SelectionSort.java
@@ -1,6 +1,8 @@
 import utils.ComparableContent;
 import utils.List;
 
+import java.util.concurrent.Callable;
+
 public class SelectionSort<ContentType extends ComparableContent<ContentType>> {
 
     private List<ContentType> list;
@@ -10,23 +12,81 @@ public class SelectionSort<ContentType extends ComparableContent<ContentType>> {
     }
 
     public void selectionSort() {
-        ContentType m = null;
-        ContentType uno = null;
-        ContentType dos = null;
         int i = 0;
+        int x = 0;
+        int y = 0;
+        int l = 0;
+        boolean ok = false;
+        ContentType lol = null;
+
         list.toFirst();
-        while (list.hasAccess()){
-            if(list.hasAccess()) {
-                uno = list.getContent();
+        while(list.hasAccess()){
+            x = 0;
+            i = x;
+            list.toFirst();
+            l = y;
+            while (l > 0){
+                list.next();
+                l--;
             }
-            list.next();
-            if(list.hasAccess()) {
-                dos = list.getContent();
+            lol = list.getContent();
+            while(ok == false){
+                if(lol.isGreater(list.getContent())){
+                    lol = list.getContent();
+                    list.next();
+                    x++;
+                    i = x;
+                    if(list.getContent() == null){
+                        ok = true;
+                    }
+                } else if(lol.isEqual(list.getContent())){
+                    list.next();
+                    if(list.getContent() == null){
+                        list.toLast();
+                        list.next();
+                        ok = true;
+                        lol = null;
+                    }
+                    x++;
+                } else if(lol.isLess(list.getContent())){
+                    list.next();
+                    if(list.getContent() == null){
+                        ok = true;
+                    }
+                    x++;
+                }
             }
-            if(uno.isGreater(dos)){
-                m = dos;
+            if (!(lol == null)) {
+                ok = false;
+                list.toFirst();
+                while (i - 1 + y > 0) {
+                    list.next();
+                    i--;
+                }
+                i = x;
+                list.remove();
+                y++;
+                l = y;
+
+                list.toFirst();
+                while (l > 1) {
+                    list.next();
+                    l--;
+                }
+                list.insert(lol);
             }
 
         }
+
+    }
+
+    public int length(){
+        int y = 1;
+        list.toFirst();
+        while(list.hasAccess()){
+            list.next();
+            y++;
+        }
+        return y;
     }
 }
