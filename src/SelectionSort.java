cfccd4abfe1bd6159038afbda9171e1a110f4d4f
zdzieczu <zuzanna.zdziech@frg.wwschool.de>
2025-02-06 11:14:46 +0100

    Selection Sort

--- src/SelectionSort.java
+++ src/SelectionSort.java
@@ -10,6 +10,41 @@ public class SelectionSort<ContentType extends ComparableContent<ContentType>> {
     }
 
     public void selectionSort() {
+        for (int i = 0; i < laengeList(list) - 1; i++) {
+            list.toFirst();
+            for (int k = 0; k < i; k++) {
+                list.next();
+                ContentType minimum = list.getContent();
+                while (list.hasAccess()) {
+                    if (list.getContent().isLess(minimum)) {
+                        minimum = list.getContent();
+                    }
+                    list.next();
+                }
 
+                list.toFirst();
+                for (int j = 0; j < i; j++) {
+                    list.next();
+                    ContentType item1 = list.getContent();
+                    list.setContent(minimum);
+                    list.next();
+                    while (list.hasAccess()) {
+                        if (list.getContent().equals(minimum)) {
+                            list.setContent(item1);
+                        } else list.next();
+                    }
+                }
+            }
+        }
+    }
+    public int laengeList (List < ContentType > list) {
+        if (list.isEmpty() || list == null) return 0;
+        int laenge = 0;
+        list.toFirst();
+        while (list.hasAccess()) {
+            laenge++;
+            list.next();
+        }
+        return laenge;
     }
 }
